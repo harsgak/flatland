ab211fa700124eaa024c9ac7b91f24763f22f4e4
Yash <ykjawale@gmail.com>
2017-09-24 00:26:30 +0530

    Added ray_hit function

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def ray_coord(coord,o):
     x, y = coord
     coord_lst = []
     if abs(o) < np.pi/2:
-        for i in range(x+1,x_max+1):
+        for i in range(x+1,x_max):
             coord_lst.append([i,y+int((i-x)*np.tan(o))])
     else:
         for i in range(0,x):
@@ -35,4 +35,22 @@ def ray_coord(coord,o):
 
 ray_coord([10,10],0.7857) #45deg
 ray_coord([10,10],2.3571) #135deg
+
+
+#@autojit(nopython=False)
+def ray_hit(coord,o):
+    # retruns first coloured pixel value.
+    x, y = coord
+    coord_lst = ray_coord([x,y],o)
+    for i, val in enumerate(coord_lst):
+        #print(img[val[0],val[1]])
+        if img[val[0],val[1]] != 255: # 255 = White in 8bit Binary_Image
+            ans = img[val[0],val[1]]
+            break
+        else:
+            ans = 255
+    return ans
+	
+a=ray_hit([10,10],2.357) #135
+a=ray_hit([10,10],0.7857) #45
 
