4623448ad47d355a140efc75f6777c9f0cd1404e
harsgak <harsgak@yahoo.com>
2017-09-24 02:55:31 +0530

    Fixed glitch and renamed [o --> theta] ~fixed glitch in ray_hit()

--- main.py
+++ main.py
@@ -33,19 +33,19 @@ import numpy as np
 
 
 @autojit(nopython=False)
-def ray_coord(coord,o):
-    # for ray_([x,y],o) returns co-ordinates of pixels of on that ray
+def ray_coord(coord,theta):
+    # for ray_([x,y],theta) returns co-ordinates of pixels on that ray
     x, y = coord
     coord_lst = []
-    if abs(o) < np.pi/2:
+    if abs(theta) < np.pi/2:
         for i in range(x+1,x_max):
-            temp_y = y+int((i-x)*np.tan(o))
+            temp_y = y+int((i-x)*np.tan(theta))
             if temp_y >= y_max:
                 break
             coord_lst.append([i,temp_y])
     else:
         for i in range(0,x):
-            temp_y = y-1+int((x-i)*np.tan(o))
+            temp_y = y-1+int((x-i)*np.tan(theta))
             if temp_y < 0:
                 break
             coord_lst.append([i,temp_y])
@@ -57,30 +57,29 @@ ray_coord([10,10],2.3571) #135deg
 
 
 #@autojit(nopython=False)
-def ray_hit(coord,o):
+def ray_hit(coord,theta):
     # retruns first coloured pixel value.
     x, y = coord
-    coord_lst = ray_coord([x,y],o)
+    coord_lst = ray_coord([x,y],theta)
+    ans = 255    #default value
     for i, val in enumerate(coord_lst):
         if img[val[0],val[1]] != 255: # 255 = White in 8bit Binary_Image
             ans = img[val[0],val[1]]
             break
-        else:
-            ans = 255
     return ans
 
 a=ray_hit([10,10],2.357) #135
 a=ray_hit([10,10],0.7857) #45
 
 
-def angle_view(coord,o_range,o_res):
+def angle_view(coord,theta_range,theta_res):
     # returns array of first coloured pixel value for theta limits.
-    # o_res = scale_resolution ie. 360*x
-    o_min, o_max = o_range # min & max - convention should be followed always
+    # theta_res = scale_resolution ie. 360*x
+    theta_min, theta_max = theta_range # min & max - convention should be followed always
     out = []
-    for theta in np.linspace(o_min,o_max,(o_max-o_min)*100*o_res):
+    for theta in np.linspace(theta_min,theta_max,(theta_max-theta_min)*100*theta_res):
         out.append(ray_hit(coord,theta))
     return np.array(out)
 	
-a=angle_view([10,10],[0.7857,2.357],0.1) #some_error_???
+a=angle_view([10,10],[0,2*np.pi],0.1)
 
